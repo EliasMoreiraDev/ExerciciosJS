b64ac6c658fdb43a087cb67a9190b4aa81124c09
Elias Moreira <121322652+EliasMoreiraDev@users.noreply.github.com>
2023-01-11 13:26:23 -0300

    11/01/2023
    Estudos de JS

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 155.7
+
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if(vel>60){
+    console.log('Velocidade permitida utrapassada. MUTADO!')
+}
+console.log('Dirija sempre com cinto de segurança')
