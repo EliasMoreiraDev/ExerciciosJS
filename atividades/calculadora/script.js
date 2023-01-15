1bb019f0da3e167b8e5cc1f37474d15bdcd8ac42
Elias Moreira <121322652+EliasMoreiraDev@users.noreply.github.com>
2023-01-15 20:44:30 -0300

    15/01/2023

---
+++ atividades/calculadora/script.js
@@ -0,0 +1,19 @@
+
+var botao = document.getElementsByClassName('botao')
+
+var res = document.getElementById('res')
+
+var item = document.createElement('option')
+
+
+function adicionar(n){   
+        res.innerHTML += n
+        valores.push(n)
+}
+function limpar(){
+        res.innerHTML = ''
+    
+}
+function calcular(){
+        
+}
